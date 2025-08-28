eb429c9ea9693a12852e3f00fb3dff23a5efa646
Emily <emily.kedenburg@gmail.com>
2025-08-28 11:30:03 -0500

    Added profiler.py

---
+++ ImageProcessorApp/profiler.py
@@ -0,0 +1,5 @@
+# This file with profile execution time, funcation call frequency, & function call hierarchy
+import time # execution time
+import cProfile # function call frequency
+import pstats # funtion call frequency
+import pyinstrument # function call hierarchy
